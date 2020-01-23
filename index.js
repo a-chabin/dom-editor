9b340f981ea4a26593bdb79b6f917b4116ce189a
Alexander Chabin <chabin@skbkontur.ru>
2020-01-23 19:44:44 +0500

    Add the jQuery parser

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ const { JSDOM } = require('jsdom');
 const cheerio = require('cheerio');
 const htmlBeautify = require('js-beautify').html;
 const isFragment = process.argv.includes('-fragment');
+const isJQuery = process.argv.includes('-jquery');
 
 const INPUT_DIR = './src/';
 const OUTPUT_DIR = './dist/';
 
-const HTML_FILENAME = 'original.html';
-const DOM_SCRIPT_FILENAME = 'instructions.js';
 const RESULT_FILENAME = 'result.html';
+const DOM_SCRIPT_FILENAME = 'instructions.js';
+const HTML_FILENAME = 'original.html';
 
 let window = null;
+let $ = null;
 let resultHTML = null;
 
 fs.watch(INPUT_DIR, { persistent: true }, () => {
@@ -19,12 +21,24 @@ fs.watch(INPUT_DIR, { persistent: true }, () => {
     const DOMScript = fs.readFileSync(INPUT_DIR + DOM_SCRIPT_FILENAME).toString();
 
     try {
-        window = (new JSDOM(html, { runScripts: "outside-only" })).window;
-        window.eval(DOMScript);
+        if (isJQuery) {
+            const $ = cheerio.load(html, {
+                withDomLvl1: isFragment,
+                decodeEntities: false
+            });
+            eval(DOMScript);
 
-        resultHTML = isFragment
-            ? window.document.body.innerHTML
-            : window.document.documentElement.outerHTML;
+            resultHTML = isFragment
+                ? $('body').html()
+                : $.html();
+        } else {
+            window = (new JSDOM(html, { runScripts: "outside-only" })).window;
+            window.eval(DOMScript);
+
+            resultHTML = isFragment
+                ? window.document.body.innerHTML
+                : window.document.documentElement.outerHTML;
+        }
 
         fs.writeFileSync(OUTPUT_DIR + RESULT_FILENAME, htmlBeautify(resultHTML));
         console.log('Transformed!');
